a1f8d969171f8e9ad3e7010f105c7f264e8b1b5a
LESHA <belochuvach@gmail.com>
2022-11-02 21:01:17 +0300

    Реализовать Store

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -14,21 +14,16 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public boolean replace(String id, T model) {
-        boolean rsl = false;
-        if (storage.get(id) != null) {
+        if (storage.containsKey(id)) {
             storage.put(id, model);
-            rsl = true;
         }
-        return rsl;
+        return storage.containsValue(model);
     }
 
     @Override
     public boolean delete(String id) {
-        boolean rsl = false;
-        if (storage.get(id) != null) {
-            storage.remove(id);
-            rsl = true;
-        }
+        boolean rsl = storage.containsKey(id);
+        storage.remove(id);
         return rsl;
     }
 
