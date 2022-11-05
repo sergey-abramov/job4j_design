4349bd922e1644dd74d1d518bde0154cc1c4424c
LESHA <belochuvach@gmail.com>
2022-11-05 17:15:15 +0300

    Очередь на двух стеках

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,19 +3,23 @@ package ru.job4j.collection;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int size;
+    private int sizeIn;
+    private int sieOut;
 
     public T poll() {
+        if (sieOut == 0) {
+            while (sizeIn != 0) {
+                out.push(in.pop());
+                sizeIn--;
+                sieOut++;
+            }
+        }
+        sieOut--;
         return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        size++;
-        for (int i = size; i > 0; i--) {
-            T rsl = in.pop();
-            size--;
-            out.push(rsl);
-        }
+        sizeIn++;
     }
 }
