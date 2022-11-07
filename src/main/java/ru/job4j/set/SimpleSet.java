a03aa98d9d758e81b35453f580d0934e2583b511
LESHA <belochuvach@gmail.com>
2022-11-07 22:17:35 +0300

    Реализовать коллекцию Set на массиве

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,61 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        boolean rsl = false;
+        if (set.size() == 0) {
+            set.add(value);
+            rsl = true;
+        } else {
+            for (int i = 0; i < set.size(); i++) {
+                if (!Objects.equals(set.get(i), value)) {
+                    set.add(value);
+                    rsl = true;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        boolean rsl = false;
+        for (int i = 0; i < set.size(); i++) {
+            rsl = Objects.equals(set.get(i), value);
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o || o == null) {
+            return true;
+        }
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleSet<?> simpleSet = (SimpleSet<?>) o;
+        return Objects.equals(set, simpleSet.set);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(set);
+    }
+}
