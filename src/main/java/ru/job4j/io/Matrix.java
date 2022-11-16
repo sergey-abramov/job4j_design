3bddba3cee73674e936f13fd90d656c77beeb158
LESHA <belochuvach@gmail.com>
2022-11-16 11:23:34 +0300

    FileOutputStream

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+        int size = 10;
+        try (FileOutputStream out = new FileOutputStream("multiple.txt")) {
+            for (int i = 1; i < size; i++) {
+                for (int j = 1; j < size; j++) {
+                    String r = Integer.toString(i * j);
+                    out.write(r.getBytes());
+                    out.write(" ".getBytes());
+                    if (j == size - 1) {
+                        out.write(System.lineSeparator().getBytes());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
