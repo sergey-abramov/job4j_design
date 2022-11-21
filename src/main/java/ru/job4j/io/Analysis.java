b67e9fee225b6abf002829b4783cf8dd6806f8da
LESHA <belochuvach@gmail.com>
2022-11-21 16:44:57 +0300

    Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -7,8 +7,8 @@ public class Analysis {
 
     public void unavailable(String source, String target) {
         boolean log = true;
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            BufferedReader in = new BufferedReader(new FileReader(source));
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target));
+            BufferedReader in = new BufferedReader(new FileReader(source))) {
             while (in.ready()) {
                 String[] s = in.readLine().split(" ");
                 if (log && (s[0].contains("400") || s[0].contains("500"))) {
@@ -16,7 +16,7 @@ public class Analysis {
                     out.printf("%s; ", s[1]);
                 } else if (!log && (s[0].contains("300") || s[0].contains("200"))) {
                     log = true;
-                    out.printf("%s;\n", s[1]);
+                    out.printf("%s;\n ", s[1]);
                 }
             }
         } catch (IOException e) {
@@ -25,12 +25,12 @@ public class Analysis {
     }
 
     public static void main(String[] args) {
-        String sourse = "C:\\projects\\job4j_design\\log\\source.txt";
-        String target = "C:\\projects\\job4j_design\\log\\target.txt";
+      /*  String sourse = "C:\\projects\\job4j_design\\log\\source_.txt";
+        String target = "C:\\projects\\job4j_design\\log\\target_1.txt";
         Analysis analysis = new Analysis();
         analysis.unavailable(sourse, target);
         sourse = "C:\\projects\\job4j_design\\log\\source_2.txt";
         target = "C:\\projects\\job4j_design\\log\\target_2.txt";
-        analysis.unavailable(sourse, target);
+        analysis.unavailable(sourse, target);*/
     }
 }
