b344947075dce6c6d526970b453a3d62a41269b9
LESHA <belochuvach@gmail.com>
2022-11-21 16:45:55 +0300

    Тестирование IO

---
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AnalysisTest {
+
+    @Test
+    void drop(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source_33.txt").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("""
+                    200 10:56:01
+                    500 10:57:01
+                    400 10:58:01
+                    300 10:59:01
+                    500 11:01:02
+                    200 11:02:02""");
+        }
+        File target  = tempDir.resolve("target_33.txt").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat("10:57:01; 10:59:01; 11:01:02; 11:02:02; ").isEqualTo(rsl.toString());
+    }
+
+    @Test
+    void drop_2(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("source_22.txt").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("300 11:02:02");
+        }
+        File target  = tempDir.resolve("target_22.txt").toFile();
+        Analysis analysis = new Analysis();
+        analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat("10:57:01; 11:02:02; ").isEqualTo(rsl.toString());
+    }
+}
