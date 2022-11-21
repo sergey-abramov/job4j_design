92f6035c8af44375b6fd34539e3babaa55ee034d
LESHA <belochuvach@gmail.com>
2022-11-21 15:24:32 +0300

    Анализ доступности сервера

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,25 +8,20 @@ public class Analysis {
     public void unavailable(String source, String target) {
         boolean log = true;
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-                while (in.ready()) {
-                    String s = in.readLine();
-                    if (log && (s.contains("400") || s.contains("500"))) {
-                        log = false;
-                        out.print(s.replaceAll("\\d{3}\\s", "") + "; ");
-                    } else if (!log && (s.contains("300") || s.contains("200"))) {
-                        log = true;
-                        out.println(s.replaceAll("\\d{3}\\s", "") + "; ");
-                    }
+            BufferedReader in = new BufferedReader(new FileReader(source));
+            while (in.ready()) {
+                String[] s = in.readLine().split(" ");
+                if (log && (s[0].contains("400") || s[0].contains("500"))) {
+                    log = false;
+                    out.printf("%s; ", s[1]);
+                } else if (!log && (s[0].contains("300") || s[0].contains("200"))) {
+                    log = true;
+                    out.printf("%s;\n", s[1]);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     public static void main(String[] args) {
