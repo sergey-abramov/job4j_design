ce64085eedf312107d3665e2cfab130f100154b7
LESHA <belochuvach@gmail.com>
2022-11-23 00:08:01 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("C:"), new DuplicatesVisitor(new FileProperty(0, "source.txt")));
+        DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("C:"), duplicatesVisitor);
+        duplicatesVisitor.filePrint(duplicatesVisitor.getPaths());
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -3,23 +3,29 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private FileProperty property;
-
-    public DuplicatesVisitor(FileProperty property) {
-        this.property = property;
-    }
+    private List<Path> paths = new ArrayList<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty property = new FileProperty(0, "source.txt");
         if (property.getName().equals(file.getFileName().toString()) && property.getSize() == file.toFile().length()) {
-            System.out.println(file.toAbsolutePath());
+            paths.add(file.toAbsolutePath());
         }
         return super.visitFile(file, attrs);
     }
 
+    public void filePrint(List<Path> paths) {
+        System.out.println(paths);
+    }
 
+    public List<Path> getPaths() {
+        return paths;
+    }
 }
