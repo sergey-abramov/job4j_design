6da1a854cb71ef87b8d8e369b3537136acb2fa8b
LESHA <belochuvach@gmail.com>
2022-11-24 22:01:07 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -12,15 +12,17 @@ import java.util.List;
 import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+
     private List<String> paths = new ArrayList<>();
+    private Map<FileProperty, String> duplicate = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        Map<FileProperty, String> duplicate = new HashMap<>();
         FileProperty property = new FileProperty(file.toFile().length(), file.toFile().getName());
         String path = duplicate.put(property, file.toFile().getAbsolutePath());
         if (path != null) {
             paths.add(path);
+            paths.add(file.toFile().getAbsolutePath());
         }
         return super.visitFile(file, attrs);
     }
