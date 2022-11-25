ddb248ce36ed8046a782ccbc674469d89f9e6ab3
LESHA <belochuvach@gmail.com>
2022-11-25 22:38:27 +0300

    Поиск дубликатов

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -3,7 +3,6 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
@@ -13,21 +12,21 @@ import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private List<String> paths = new ArrayList<>();
-    private Map<FileProperty, String> duplicate = new HashMap<>();
+    private final Map<FileProperty, List<Path>> duplicate = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty property = new FileProperty(file.toFile().length(), file.toFile().getName());
-        String path = duplicate.put(property, file.toFile().getAbsolutePath());
-        if (path != null) {
-            paths.add(path);
-            paths.add(file.toFile().getAbsolutePath());
-        }
+        FileProperty property = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        duplicate.putIfAbsent(property, new ArrayList<>());
+        duplicate.get(property).add(file.toAbsolutePath());
         return super.visitFile(file, attrs);
     }
 
     public void filePrint() {
-        System.out.println(paths);
+        List<Path> list = new ArrayList<>();
+        duplicate.values().stream()
+                .filter(paths -> paths.size() > 1)
+                .forEach(list :: addAll);
+        list.forEach(System.out::println);
     }
 }
