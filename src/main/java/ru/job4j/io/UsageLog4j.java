3a4222bcf525eea3f70905fc8c1c60bbec6d0b69
LESHA <belochuvach@gmail.com>
2022-12-09 12:33:04 +0300

    Slf4j - вывод переменных.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -13,5 +13,16 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        String name = "Sergey";
+        int age = 24;
+        boolean yng = true;
+        long growthMM = 17500;
+        double growthM = 1.75;
+        float growtM = 1.75f;
+        byte byt = 2;
+        short s = 23;
+        LOG.debug("User info name : {}, age : {}, growthMM : {}, growthM : {}, growtM {}.",
+                name, age, growthMM, growthM, growtM);
+        LOG.debug("Yng? - {}. Byt = {}, short = {}", yng, byt, s);
     }
 }
