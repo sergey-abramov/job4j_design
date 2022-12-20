f71107a2cdd783e2c117d56b19601509b44eccb2
LESHA <belochuvach@gmail.com>
2022-12-20 17:06:38 +0300

    Схема прав пользователи и роли.

--- sql/create.sql
+++ sql/create.sql
@@ -1,12 +1,42 @@
-create table role(
+create table comments(
 	id serial primary key,
 	name varchar(255)
+);
+
+create table attachs(
+	id serial primary key,
+	name varchar(255)
+);
+
+create table item(
+	id serial primary key,
+	name varchar(255),
+	comments_id int references comments(id),
+	attachs_id int references attachs(id)
+);
+
+create table category(
+	id serial priimary key,
+	name varchar(255),
+	item_id int references item(id)
+);
+
+create table state(
+	id serial primary key,
+	status varchar(255)
+	item_id int references item(id)
 );
 
 create table users(
 	id serial primary key,
 	name varchar(255),
-	role_id int references role(id)
+	item_id int references item(id)
+);
+
+create table role(
+	id serial primary key,
+	name varchar(255),
+	users_id int references users(id)
 );
 
 create table rules(
@@ -18,34 +48,4 @@ create table role_rules(
 	id serial primary key,
 	role_id int references role(id),
 	rules_id int references rules(id)
-);
-
-create table item(
-	id serial primary key,
-	name varchar(255),
-	users_id int references user(id)
-);
-
-create table comments(
-	id serial primary key,
-	name varchar(255),
-	item_id int references item(id)
-);
-
-create table attachs(
-	id serial primary key,
-	name varchar(255),
-	item_id int references item(id)
-);
-
-create table category(
-	id serial priimary key,
-	name varchar(255),
-	item_id int references item(id) unique
-);
-
-create table state(
-	id serial primary key,
-	status varchar(255)
-	item_id int references item(id) unique
 );
