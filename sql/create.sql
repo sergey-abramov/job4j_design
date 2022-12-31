db3e2873cdaff4d097c660d7fb9fa711348c4495
LESHA <belochuvach@gmail.com>
2022-12-31 10:48:37 +0300

    Схема прав пользователи и роли.

--- sql/create.sql
+++ sql/create.sql
@@ -1,42 +1,22 @@
-create table comments(
-	id serial primary key,
-	name varchar(255)
-);
-
-create table attachs(
-	id serial primary key,
-	name varchar(255)
-);
-
-create table item(
-	id serial primary key,
-	name varchar(255),
-	comments_id int references comments(id),
-	attachs_id int references attachs(id)
-);
-
 create table category(
 	id serial priimary key,
-	name varchar(255),
-	item_id int references item(id)
+	name varchar(255)
 );
 
 create table state(
 	id serial primary key,
 	status varchar(255)
-	item_id int references item(id)
 );
 
 create table users(
 	id serial primary key,
 	name varchar(255),
-	item_id int references item(id)
+	role_id int references role(id)
 );
 
 create table role(
 	id serial primary key,
-	name varchar(255),
-	users_id int references users(id)
+	name varchar(255)
 );
 
 create table rules(
@@ -48,4 +28,23 @@ create table role_rules(
 	id serial primary key,
 	role_id int references role(id),
 	rules_id int references rules(id)
+);
+create table item(
+	id serial primary key,
+	name varchar(255),
+	category_id int references category(id),
+	state_id int references state(id),
+	users_id int references users(id)
+);
+
+create table comments(
+	id serial primary key,
+	name varchar(255),
+	item_id int references item(id)
+);
+
+create table attachs(
+	id serial primary key,
+	name varchar(255),
+	item_id int references item(id)
 );
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,23 +1,23 @@
-insert into item(name, comments_id, attachs_id) values ('Item 1', 1, 2);
-insert into item(name, comments_id, attachs_id) values ('Item 2', 2, 1);
+insert into item(name, caregory_id, state_id, users_id) values ('Item 1', 1, 1, 1);
+insert into item(name, caregory_id, state_id, users_id) values ('Item 2', 1, 1, 1);
 
-insert into comments(name) values ('Comment 2');
-insert into comments(name) values ('Comment 1');
+insert into comments(name, item_id) values ('Comment 2', 1);
+insert into comments(name, item_id) values ('Comment 1', 1);
 
-insert into attachs(name) values ('File.txt');
-insert into attachs(name) values ('File2.txt');
+insert into attachs(name, item_id) values ('File.txt', 1);
+insert into attachs(name, item_id) values ('File2.txt', 1);
 
-insert into category(name, item_id) values ('Category 1', 1);
-insert into category(name, item_id) values ('Category 2', 1);
+insert into category(name) values ('Category 1');
+insert into category(name) values ('Category 2');
 
-insert into state(status, item_id) values ('Filed', 1);
-insert into state(status, item_id) values ('Filed', 3);
+insert into state(status) values ('Filed');
+insert into state(status) values ('Filed');
 
-insert into role(name, users_id) values ('Hero', 1);
-insert into role(name, users_id) values ('AntiHero', 2);
+insert into role(name) values ('Hero');
+insert into role(name) values ('AntiHero');
 
-insert into users(name, item_id) values ('Ivan', 1);
-insert into users(name, item_id) values ('Vladimir', 2);
+insert into users(name, role_id) values ('Ivan', 1);
+insert into users(name, role_id) values ('Vladimir', 1);
 
 insert into rules(name) values ('Ivan');
 insert into rules(name) values ('Ivan');
