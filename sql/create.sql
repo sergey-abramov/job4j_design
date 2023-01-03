15c4a1f270666adf69c69bee0efcf7cdb5714cd4
LESHA <belochuvach@gmail.com>
2023-01-03 18:14:41 +0300

    Схема прав пользователи и роли.

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,5 @@
 create table category(
-	id serial priimary key,
+	id serial primary key,
 	name varchar(255)
 );
 
@@ -8,15 +8,15 @@ create table state(
 	status varchar(255)
 );
 
+create table role(
+	id serial primary key,
+	name varchar(255)
+);
+
 create table users(
 	id serial primary key,
 	name varchar(255),
 	role_id int references role(id)
-);
-
-create table role(
-	id serial primary key,
-	name varchar(255)
 );
 
 create table rules(
--- sql/insert.sql
+++ sql/insert.sql
@@ -1,12 +1,3 @@
-insert into item(name, caregory_id, state_id, users_id) values ('Item 1', 1, 1, 1);
-insert into item(name, caregory_id, state_id, users_id) values ('Item 2', 1, 1, 1);
-
-insert into comments(name, item_id) values ('Comment 2', 1);
-insert into comments(name, item_id) values ('Comment 1', 1);
-
-insert into attachs(name, item_id) values ('File.txt', 1);
-insert into attachs(name, item_id) values ('File2.txt', 1);
-
 insert into category(name) values ('Category 1');
 insert into category(name) values ('Category 2');
 
@@ -23,3 +14,12 @@ insert into rules(name) values ('Ivan');
 insert into rules(name) values ('Ivan');
 
 insert into role_rules(role_id, rules_id) values (1, 2);
+
+insert into item(name, caregory_id, state_id, users_id) values ('Item 1', 1, 1, 1);
+insert into item(name, caregory_id, state_id, users_id) values ('Item 2', 1, 1, 1);
+
+insert into comments(name, item_id) values ('Comment 2', 1);
+insert into comments(name, item_id) values ('Comment 1', 1);
+
+insert into attachs(name, item_id) values ('File.txt', 1);
+insert into attachs(name, item_id) values ('File2.txt', 1);
