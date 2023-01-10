c26ff55323bed2e70c99f4de5268cbf20e2906ce
LESHA <belochuvach@gmail.com>
2023-01-10 16:30:50 +0300

    Подзапросы

---
+++ sql/customers.sql
@@ -0,0 +1,28 @@
+CREATE TABLE customers(
+    id serial primary key,
+    first_name text,
+    last_name text,
+    age int,
+    country text
+);
+
+CREATE TABLE orders(
+    id serial primary key,
+    amount int,
+    customer_id int references customers(id)
+);
+
+insert into customers(first_name, last_name, age, country)
+values ('Desmont','Desmont1', 18, 'Rus');
+insert into customers(first_name, last_name, age, country)
+values ('Vladmir','Vova', 28, 'Eng');
+insert into customers(first_name, last_name, age, country)
+values ('Ivan','Vanya', 55, 'US');
+
+select * from customers where age = (select min(age) from customers);
+
+insert into orders(amount, customer_id) values (1, 2);
+insert into orders(amount, customer_id) values (2, 2);
+insert into orders(amount, customer_id) values (3, 1);
+
+select * from customers where customers.id not in (select customers.id from orders);
